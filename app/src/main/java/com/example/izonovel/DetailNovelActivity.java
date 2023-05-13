793ff2797e0c51b0336bc24380e46b4f33aac67e
Tegguh <Teguhadiprayoga2@gmail.com>
2023-05-13 16:25:53 +0700

    Menambahkan MenuFavoritActivity.java dan activity_daftar_novel.xml menambahkan DetailNovelActivity.java
    Nama Teguh Adi Prayoga
    UTS MP
    NIM 210444020056

---
+++ app/src/main/java/com/example/izonovel/DetailNovelActivity.java
@@ -0,0 +1,14 @@
+package com.example.izonovel;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DetailNovelActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail_novel);
+    }
+}
---
+++ app/src/main/java/com/example/izonovel/MenuFavoritActivity.java
@@ -0,0 +1,14 @@
+package com.example.izonovel;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MenuFavoritActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu_favorit);
+    }
+}
